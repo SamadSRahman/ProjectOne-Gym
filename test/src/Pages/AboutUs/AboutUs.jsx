eb12bb18e46636d6a17e2e2591e55c92bff7328b
shubham-shinde1 <103098508+shubham-shinde1@users.noreply.github.com>
2023-04-18 14:06:35 +0530

    new page

---
+++ test/src/Pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+export function AboutUs() {
+  return (
+    <Fragment>
+      <div style={{ backgroundImage: "background-image.jpg" }}></div>
+    </Fragment>
+  );
+}
