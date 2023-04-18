b72c1efaa23c3354e9c9fe80189d552a5529aed6
Uttu32 <utkarshksharm7878@gmail.com>
2023-04-18 16:29:28 +0530

    worthless

--- test/src/App.js
+++ test/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
 
     <div className="App">
-      {/* <Navbar /> */}
+      <Navbar />
       <Registration/>
       <Routes>
         {/* <Route path='/' element={<Home />}/> */}
