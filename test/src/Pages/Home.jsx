88095f79dd2e5103fb628b86a36764271c71116b
SKY1272 <MY_NAME@example.com>
2023-04-18 14:18:18 +0530

    HomeUpdated

--- test/src/Pages/Home.jsx
+++ test/src/Pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import './Home.css'
 function Home(){
   return(
-  <div>
-    <h1>Hello I am Home page</h1>
+<section class="image-section">
+  <div className='main'>
+    <main>Take care of your body it's the only place you have to live</main>
   </div>
+  <div class="image-container">
+    <img src="https://images.pexels.com/photos/1552242/pexels-photo-1552242.jpeg?auto=compress&cs=tinysrgb&w=600" alt="your-image-alt"/>
+  </div>
+  <div class="button-container">
+    <a href="#" class="button">Join us</a>
+  </div>
+</section>
   )
 }
 export default Home
