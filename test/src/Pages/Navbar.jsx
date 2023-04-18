4d1f14ff6a6037c643696b9479b24fc7420bfcf3
Uttu32 <utkarshksharm7878@gmail.com>
2023-04-18 13:10:31 +0530

    Navabar design

--- test/src/Pages/Navbar.jsx
+++ test/src/Pages/Navbar.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css"
+
 export function Navbar() {
-    
-    return (
-        <div>
-            <Link to='/'>Home</Link>
-            <Link to='/aboutus'>About Us</Link>
-            <Link to='/classes'>Classes</Link>
-            <Link to='/blog'>Blog</Link>
+  return (
+    <div className="Navbar">
+      <div className="logo">
+        <Link to="/">
+          <img 
+          src="https://www.shutterstock.com/image-vector/fitness-gym-logo-design-template-260nw-1662632119.jpg"
+          alt="logo"
+          />
+         </Link>
+      </div>
+        <div className="navi">
+          <nav>
+            <Link to="/"><span>Home</span></Link>
+            <Link to="/aboutus"><span>About Us</span></Link>
+            <Link to="/classes"><span>Classes</span></Link>
+            <Link to="/blog"><span>Blog</span></Link>
+          </nav>
         </div>
-    )
+    </div>
+  );
 }
