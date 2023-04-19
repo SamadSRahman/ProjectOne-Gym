61cf7085f2885239da9ef9070ddde0871569d34b
shubham-shinde1 <103098508+shubham-shinde1@users.noreply.github.com>
2023-04-19 12:02:46 +0530

    AboutUs-complete-ver1

--- test/src/Pages/AboutUs/AboutUs.jsx
+++ test/src/Pages/AboutUs/AboutUs.jsx
@@ -12,34 +12,52 @@ export function AboutUs() {
           <div className={Style.section1}>
             <div className={Style.aboutUs}>
               <div>
-                <img src="https://goldsgym.in/uploads/home_icons/gyms.jpg" />
+                <img
+                  className={Style.sideBarImg}
+                  src="https://goldsgym.in/uploads/home_icons/gyms.jpg"
+                />
                 <h3>150 Gyms</h3>
               </div>
 
               <div>
-                <img src="https://goldsgym.in/uploads/home_icons/cities.jpg" />
+                <img
+                  className={Style.sideBarImg}
+                  src="https://goldsgym.in/uploads/home_icons/cities.jpg"
+                />
                 <h3>95 Cities</h3>
               </div>
 
               <div>
-                <img src="https://goldsgym.in/uploads/home_icons/states.jpg" />
+                <img
+                  className={Style.sideBarImg}
+                  src="https://goldsgym.in/uploads/home_icons/states.jpg"
+                />
                 <h3>25 States</h3>
               </div>
 
               <div>
-                <img src="https://goldsgym.in/uploads/home_icons/Website_Personal.jpg" />{" "}
+                <img
+                  className={Style.sideBarImg}
+                  src="https://goldsgym.in/uploads/home_icons/Website_Personal.jpg"
+                />{" "}
                 <h3>Personal Traning</h3>
                 <h3>Program</h3>
               </div>
 
               <div>
-                <img src="https://goldsgym.in/uploads/home_icons/Corporate.jpg" />
+                <img
+                  className={Style.sideBarImg}
+                  src="https://goldsgym.in/uploads/home_icons/Corporate.jpg"
+                />
                 <h3>Corporate Wellness </h3>
                 <h3> Program</h3>
               </div>
 
               <div>
-                <img src="https://goldsgym.in/uploads/home_icons/Group_Exercise.jpg" />
+                <img
+                  className={Style.sideBarImg}
+                  src="https://goldsgym.in/uploads/home_icons/Group_Exercise.jpg"
+                />
                 <h3>Group Program</h3>
               </div>
             </div>
@@ -49,13 +67,13 @@ export function AboutUs() {
             <div class={Style.mainDiscription}>
               <div class={Style.discription}>
                 <div class={Style.image}>
-                  <img src="https://goldsgym.in/assets/images/work.jpg" />
+                  <img
+                    className={Style.imagelady}
+                    src="https://goldsgym.in/assets/images/work.jpg"
+                  />
                 </div>
-                <h3>What Do We Do</h3>
-                <br></br>
-                <br></br>
-                <br></br>
-                <p>
+                <h3 className={Style.discriptionHeading}>What Do We Do</h3>
+                <p className={Style.para1}>
                   Gold's Gym Has Been The Authority In Fitness Since 1965 Dating
                   Back To The Original Gold's Gym In Venice, California. It Was
                   The Place For Serious Fitness. Opened Long Before The
@@ -64,10 +82,8 @@ export function AboutUs() {
                   Results. It Was An Instant Hit. Gold's Gym Quickly Became
                   Known As “The Mecca Of Bodybuilding.''
                 </p>
-                <br></br>
-                <br></br>
-                <br></br>
-                <p>
+
+                <p className={Style.para1}>
                   In 1977, Gold's Gym Received International Attention When It
                   Was Featured In The Movie Pumping Iron That Starred Arnold
                   Schwarzenegger And Lou Ferrigno. From That First Gym In
@@ -78,10 +94,8 @@ export function AboutUs() {
                   Personal Training, Cardiovascular Equipment, Spinning, And
                   Yoga, While Maintaining Its Core Weight Lifting Tradition.
                 </p>
-                <br></br>
-                <br></br>
-                <br></br>
-                <p>
+
+                <p className={Style.para1}>
                   Gold's Gym Has Become The Preferred Gym Of Celebrities,
                   Athletes, Bodybuilders, The Military, And Fitness Enthusiasts
                   All Over The World. Most Importantly, Gold's Gym Continues To
