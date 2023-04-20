af10bfb092a05b780df2e1a2b3e9a805a8dc4a75
SamadSRahman <samadsrahman@gmail.com>
2023-04-20 10:34:04 +0530

    Navbar Issues Resolved

--- test/src/data/blogData.js
+++ test/src/data/blogData.js
@@ -5,12 +5,12 @@ export const blogBgImg = [
 
     export const blogData = [{
         img: "https://images.pexels.com/photos/3490363/pexels-photo-3490363.jpeg?auto=compress&cs=tinysrgb&w=800",
-        text: "Fitness Power has been awesome to me. Their trainers are exceptional, the overall vibe is great and I have had nothing but good experiences. I definitely recommend them to my friends and family.",
+        text: "Power Fitness has been awesome to me. Their trainers are exceptional, the overall vibe is great and I have had nothing but good experiences. I definitely recommend them to my friends and family.",
         name: "John"
     },
         {
             img: "https://images.pexels.com/photos/2105493/pexels-photo-2105493.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-            text:  " Hands down, best choice I’ve made training at Fitness Power. VERY friendly team who make me actually want to go train. I look forward to walking through the doors and always feel amazing when I leave.",name:"Manish"
+            text:  " Hands down, best choice I’ve made training at Power Fitness. VERY friendly team who make me actually want to go train. I look forward to walking through the doors and always feel amazing when I leave.",name:"Manish"
         }, 
         {
             img: "https://images.pexels.com/photos/3775576/pexels-photo-3775576.jpeg?auto=compress&cs=tinysrgb&w=800",
@@ -20,7 +20,7 @@ export const blogBgImg = [
             text:  "Excellent personal training. Trainers who really care and go above and beyond to help you achieve your goals. Small and private gym that feels more personal. Highly recommended!", name:"Shruti"
         }, {
             img: "https://images.pexels.com/photos/2036998/pexels-photo-2036998.jpeg?auto=compress&cs=tinysrgb&w=800",
-            text: " By far the best facility around. This new location is mint from the staff, to the equipment, to the facility itself. From the ownership, to management, to the trainers, by far the nicest and most knowledgeable staff in the business today. I wouldn’t hesitate to recommend (GN) to anyone.",
+            text: " By far the best facility around. This new location is mint from the staff, to the equipment, to the facility itself. From the ownership, to management, to the trainers, by far the nicest and most knowledgeable staff in the business today. I wouldn’t hesitate to recommend Power Fitness to anyone.",
             name:"Sara"
         }, {
             img: "https://images.pexels.com/photos/6550866/pexels-photo-6550866.jpeg?auto=compress&cs=tinysrgb&w=800",
